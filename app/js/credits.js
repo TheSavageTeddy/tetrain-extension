05fb5961dd21e5da8daa1a3d42578e1e0e52826a
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-08-21 08:04:45 +0800

    credits error js fix

---
+++ app/js/credits.js
@@ -0,0 +1,20 @@
+function goBack(){
+    window.location.replace("../../popup.html");
+}
+function keydown(ev){
+    switch (ev.keyCode) {
+        case 66:
+            goBack();
+        case 37:
+            goBack();
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('back').addEventListener('click', goBack);
+
+
+    document.addEventListener('keydown', keydown, false);
+});
